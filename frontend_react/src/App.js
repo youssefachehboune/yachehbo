dc2fff41fee4862e092ba6a793e02ae019066d16
Youssef Achehboune <achehboun949@gmail.com>
2022-09-19 15:57:50 +0200

    Add Navbar

--- frontend_react/src/App.js
+++ frontend_react/src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
 import { About, Footer, Header, Skills, Testimonial, Work } from './container';
-import { Navber } from './components';
+import { Navbar } from './components';
 import './App.scss';
 
 const App = () => {
   return (
     <div className='app'>
-        <Navber />
+        <Navbar />
         <Header />
         <About />
         <Work />
