22e1f856ec2493b8346c5156d9203d8cf19320f5
Youssef Achehboune <achehboun949@gmail.com>
2022-09-20 20:43:14 +0200

    Add about section

--- frontend_react/src/constants/images.js
+++ frontend_react/src/constants/images.js
@@ -6,7 +6,10 @@ import profile from '../assets/yachehbo.png';
 import circle from '../assets/circle.svg';
 import logo from '../assets/logo.png';
 
-
+import about01 from '../assets/about01.png';
+import about02 from '../assets/about02.png';
+import about03 from '../assets/about03.png';
+import about04 from '../assets/about04.png';
 export default {
   figma,
   python,
@@ -14,4 +17,8 @@ export default {
   profile,
   circle,
   logo,
+  about01,
+  about02,
+  about03,
+  about04
 };
--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -1,10 +1,52 @@
-import React from 'react'
+import React, { useState, useEffect} from 'react';
+import { motion } from 'framer-motion';
+
+import {images} from '../../constants';
 
 import './About.scss';
 
+import { urlFor, client } from '../../client';
+
 const About = () => {
+  const [abouts, setAbouts] = useState([]);
+
+  useEffect(() => {
+    const query = '*[_type == "abouts"]'
+
+    client.fetch(query)
+      .then((data) => setAbouts(data))
+  }, [])
+  
   return (
-    <div>About</div>
+    <>
+      <motion.h2 
+        className="head-text"
+        whileInView={{ x: [-100, 1, 0], opacity: [0, 1] }}
+        transition={{ duration: 2, type: 'tween' }}
+        >
+        I'm just a
+        <span> Developer </span>
+        <br />
+        who enjoys
+        <span> Coding</span>
+      </motion.h2>
+
+      <div className="app__profiles">
+        {abouts.map((about, index) => (
+          <motion.div
+            whileInView={{ y: [50, -5, 0], opacity: [0, 1] }}
+            whileHover={{ scale: 1.1 }}
+            //transition={{ duration: 1, type: 'tween' }}
+            className="app__profile-item"
+            key={about.title + index}
+          >
+              <img src={urlFor(about.imgUrl)} alt={about.title} />
+              <h2 className="bold-text" style={{ marginTop: 20 }}>{about.title}</h2>
+              <p className="p-text" style={{ marginTop: 10 }}>{about.description}</p>
+          </motion.div>
+        ))}
+      </div>
+    </>
   );
 }
 
